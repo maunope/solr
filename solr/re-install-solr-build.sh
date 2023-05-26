5f3514840d639ddc8aef7328aef8c055db3393ae
maunope <mnoseda@google.com>
2023-05-26 13:01:23 +0200

    enabled local backup

--- solr/re-install-solr-build.sh
+++ solr/re-install-solr-build.sh
@@ -5,6 +5,20 @@ export N_WORKERS=2
 max_m=$(($N_MASTERS - 1))
 
 
+gcloud compute ssh ${CLUSTER_NAME}-m-0 --zone europe-west3-b -- << EOF
+
+sudo apt install nfs-kernel-server
+sudo mkdir /mnt/solrbackupsshare
+sudo chown nobody:nogroup  /mnt/solrbackupsshare
+sudo chmod 777 nobody:nogroup  /mnt/solrbackupsshare
+sudo chmod 777   /mnt/solrbackupsshare
+sudo sed -i  's/\/mnt\/solrbackupsshare 10.0.0.0\/8(rw,sync,no_subtree_check)//' /etc/exports
+echo "/mnt/solrbackupsshare 10.0.0.0/8(rw,sync,no_subtree_check)" |sudo tee -a  /etc/exports
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+sudo systemctl restart  nfs-server
+EOF
+
 for i in `seq 0 $max_m`
 do
     echo "${CLUSTER_NAME}-m-${i}"
@@ -16,7 +30,7 @@ gsutil -m cp -r gs://chb-isp-hadoop-playground-jobs-src/solr_8.1.1/* our-solr-8.
 sudo rm /usr/lib/solr/lib/*
 sudo rm -r /usr/lib/solr/lib/solrj-lib
 sudo cp -r our-solr-8.1.1/* /usr/lib/solr/lib/
-sudo rm /usr/lib/solr/lib/solr.xml
+sh 
 
 sudo rm /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-core-8.1.1.jar
 sudo rm /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-solrj-8.1.1.jar
@@ -29,6 +43,13 @@ stat /usr/lib/solr/lib
 stat /usr/lib/solr/lib/solrj-lib
 stat /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-core-8.1.1.jar
 stat /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-solrj-8.1.1.jar
+
+
+sudo mkdir /tmp/localsolarbackupmount
+sudo mount -t ${CLUSTER_NAME}-m-0:/mnt/solrbackupsshare /tmp/localsolarbackupmount
+sudo chmod 777 /mnt/solrbackupsshare
+sudo chmode 777 /tmp/localsolarbackupmount
+
 
 sudo systemctl restart solr
 EOF
@@ -45,6 +66,7 @@ mkdir our-solr-8.1.1
 gsutil -m cp -r gs://chb-isp-hadoop-playground-jobs-src/solr_8.1.1/* our-solr-8.1.1
 
 sudo rm /usr/lib/solr/lib/*
+
 sudo rm -r /usr/lib/solr/lib/solrj-lib
 sudo cp -r our-solr-8.1.1/* /usr/lib/solr/lib/
 sudo rm /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-core-8.1.1.jar
@@ -52,10 +74,16 @@ sudo rm /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-solrj-8.1.1.jar
 sudo cp our-solr-8.1.1/solr-core-8.1.1.jar /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/
 sudo cp our-solr-8.1.1/solr-solrj-8.1.1.jar /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/
 
+sudo cp our-solr-8.1.1/solr.xml /usr/lib/solr/server/solr/solr.xml 
+
 stat /usr/lib/solr/lib
 stat /usr/lib/solr/lib/solrj-lib
 stat /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-core-8.1.1.jar
 stat /usr/lib/solr/server/solr-webapp/webapp/WEB-INF/lib/solr-solrj-8.1.1.jar
+
+sudo mkdir /tmp/localsolarbackupmount
+sudo mount -t nfs ${CLUSTER_NAME}-w-0:/mnt/solrbackupsshare /tmp/localsolarbackupmount
+sudo chmod 777 /tmp/localsolarbackupmount
 
 sudo systemctl restart solr
 EOF
--- solr/solr-backup-local.sh
+++ solr/solr-backup-local.sh
@@ -1,4 +1,4 @@
-export BCK_HDFS=file:///tmp/solrDebug/backup_data
+export BCK_SHARE=/tmp/localsolarbackupmount/
 export BCK_NAME=$(date +%s)
 
 
@@ -7,7 +7,7 @@ export BCK_NAME=$(date +%s)
 # Backup the collection
 curl -H "Content-Type: application/json" \
     -X POST \
-    --url "http://localhost:8983/solr/admin/collections?action=BACKUP&name=books_b_${BCK_NAME}&collection=books_0&repository=local&location=${BCK_HDFS}/"
+    --url "http://localhost:8983/solr/admin/collections?action=BACKUP&name=books_b_${BCK_NAME}&collection=books_0&repository=local&location=${BCK_SHARE}/"
 
 
 # Check backup size on HDFS
